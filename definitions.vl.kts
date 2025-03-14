52c2cde01b6b6a3f0b1ce6b079e65f5e3ee1c0d9
Pascal Knecht <pascal@addere.ch>
2025-03-14 14:29:35 +0100

    Apply changes for Vulnlog 0.6

--- definitions.vl.kts
+++ definitions.vl.kts
@@ -1,11 +1,15 @@
 releases {
-    branch("v1") {
+    branch("branch 1") {
         release("0.1.0", "2025-01-01")
         release("0.1.1", "2025-01-23")
         release("0.2.0")
     }
-    branch("v2") {
+    branch("branch 2") {
         release("2.0.0", "2025-02-01")
         release("2.1.0")
     }
+}
+
+reporters {
+    reporter("demo reporter")
 }
